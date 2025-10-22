da026f96e4fd5149d27e726e3a70d01e33685556
mducng12-5 <minhnguyenduc1205@gmail.com>
2025-10-22 19:07:55 +0700

    commit buoi 1

---
+++ Lecture 1 22_10_25/Lecture1.m
@@ -0,0 +1,21 @@
+% % Opening
+% a = input("Nhap he so a: ");
+% b = input("Nhap he so b: ");
+% c = input("Nhap he so c: ");
+% 
+% % Calculation
+% delta = b^2 - 4*a*c;
+% if (delta >= 0)
+%     x1 = (-b + sqrt(delta)/(2*a));
+%     x2 = (-b - sqrt(delta)/(2*a));
+%     fprintf('x1 = %d\n', x1);
+%     fprintf('x2 = %d\n', x2);
+% else
+%     fprintf('no result\n')
+% end
+
+name = input('user name: ', 's');
+fprintf("Name of user: %s\n", name);
+
+
+
