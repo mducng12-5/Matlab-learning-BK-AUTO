c5671def3eb8abeb7266f14768177a5ab902cea0
mducng12-5 <minhnguyenduc1205@gmail.com>
2025-10-27 23:36:33 +0700

    finish 1 2 3

--- Midterm test/main.m
+++ Midterm test/main.m
@@ -6,98 +6,117 @@
 %--------------------------------------------------------------------------
 
 %% Bài 1
-% x = linspace(0, 1);
-% W_L = 4400./(1.425+x);
-% 
-% plot(W_L, x)
-% 
-% title('Đồ thị W_L') 
-% xlabel('W_L')
-% ylabel('x')
-% grid on
+x = linspace(0, 1);
+W_L = 4400./(1.425+x);
+
+plot(W_L, x)
+
+title('Đồ thị W_L') 
+xlabel('W_L')
+ylabel('x')
+grid on
 
 %% Bài 2
 
+n = input("nhap kich thuoc (nxn): ")
+if mod(n,2) == 0
+    n = n+1;
+end
+
+for i = 1:n
+    for j = 1:n
+        if(i == j || i + j == n+1 || i == (n+1)/2 || j == (n+1)/2 )
+            fprintf('o')
+        else
+            fprintf(' ')
+        end
+    end
+    fprintf('\n')
+end
+
+
+
+
 %% Bài 3
 
-% while 1
-%     so_nuoc_di = 0;
-%     chess_coordinate = input('Nhap vao toa do quan co: ', "s")
-%     chess_col = chess_coordinate(1);
-%     chess_row = str2double(chess_coordinate(2:end));
-%     if chess_col == 'a'
-%         chess_col_alt = 1;
-%     elseif chess_col == 'b'
-%         chess_col_alt = 2;
-%     elseif chess_col == 'c'
-%         chess_col_alt = 3;
-%     elseif chess_col == 'd'
-%         chess_col_alt = 4;
-%     elseif chess_col == 'e'
-%         chess_col_alt = 5;
-%     elseif chess_col == 'f'
-%         chess_col_alt = 6;
-%     elseif chess_col == 'g'
-%         chess_col_alt = 7;
-%     elseif chess_col == 'h'
-%         chess_col_alt = 8;
-%     end
-% 
-%     chess_col_alt_1 = chess_col_alt + 1;
-%     chess_row_1 = chess_row + 2;
-% 
-%     chess_col_alt_2 = chess_col_alt - 1;
-%     chess_row_2 = chess_row + 2;
-% 
-%     chess_col_alt_3 = chess_col_alt - 1;
-%     chess_row_3 = chess_row - 2;
-% 
-%     chess_col_alt_4 = chess_col_alt + 1;
-%     chess_row_4 = chess_row - 2;
-% 
-%     chess_col_alt_5 = chess_col_alt + 2;
-%     chess_row_5 = chess_row + 1;
-% 
-%     chess_col_alt_6 = chess_col_alt - 2;
-%     chess_row_6 = chess_row + 1;
-% 
-%     chess_col_alt_7 = chess_col_alt - 2;
-%     chess_row_7 = chess_row - 1;
-% 
-%     chess_col_alt_8 = chess_col_alt + 2;
-%     chess_row_8 = chess_row - 1;
-% 
-%     if (chess_row < 1 || chess_row > 8) || (chess_col ~= 'a' && chess_col ~= 'b' && chess_col ~= 'c' && chess_col ~= 'd' && chess_col ~= 'e' && chess_col ~= 'f' && chess_col ~= 'g' && chess_col ~= 'h')
-%         fprintf('toa do khong hop le\n')
-%     else
-%         if chess_row_8 >= 1 && chess_row_8 <= 8 && chess_col_alt_8 >= 1 && chess_col_alt_8 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_7 >= 1 && chess_row_7 <= 8 && chess_col_alt_7 >= 1 && chess_col_alt_7 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_6 >= 1 && chess_row_6 <= 8 && chess_col_alt_6 >= 1 && chess_col_alt_6 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_5 >= 1 && chess_row_5 <= 8 && chess_col_alt_5 >= 1 && chess_col_alt_5 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_4 >= 1 && chess_row_4 <= 8 && chess_col_alt_4 >= 1 && chess_col_alt_4 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_3 >= 1 && chess_row_3 <= 8 && chess_col_alt_3 >= 1 && chess_col_alt_3 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_2 >= 1 && chess_row_2 <= 8 && chess_col_alt_2 >= 1 && chess_col_alt_2 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         if chess_row_1 >= 1 && chess_row_1 <= 8 && chess_col_alt_1 >= 1 && chess_col_alt_1 <= 8
-%             so_nuoc_di = so_nuoc_di + 1;
-%         end
-%         fprintf('So nuoc di hop le la: ')
-%         fprintf('%d\n',so_nuoc_di)
-% 
-%     end
-% end
+while 1
+    so_nuoc_di = 0;
+    chess_coordinate = input('Nhap vao toa do quan co: ', "s")
+    chess_col = chess_coordinate(1);
+    chess_row = str2double(chess_coordinate(2:end));
+    if chess_col == 'a'
+        chess_col_alt = 1;
+    elseif chess_col == 'b'
+        chess_col_alt = 2;
+    elseif chess_col == 'c'
+        chess_col_alt = 3;
+    elseif chess_col == 'd'
+        chess_col_alt = 4;
+    elseif chess_col == 'e'
+        chess_col_alt = 5;
+    elseif chess_col == 'f'
+        chess_col_alt = 6;
+    elseif chess_col == 'g'
+        chess_col_alt = 7;
+    elseif chess_col == 'h'
+        chess_col_alt = 8;
+    end
+
+    chess_col_alt_1 = chess_col_alt + 1;
+    chess_row_1 = chess_row + 2;
+
+    chess_col_alt_2 = chess_col_alt - 1;
+    chess_row_2 = chess_row + 2;
+
+    chess_col_alt_3 = chess_col_alt - 1;
+    chess_row_3 = chess_row - 2;
+
+    chess_col_alt_4 = chess_col_alt + 1;
+    chess_row_4 = chess_row - 2;
+
+    chess_col_alt_5 = chess_col_alt + 2;
+    chess_row_5 = chess_row + 1;
+
+    chess_col_alt_6 = chess_col_alt - 2;
+    chess_row_6 = chess_row + 1;
+
+    chess_col_alt_7 = chess_col_alt - 2;
+    chess_row_7 = chess_row - 1;
+
+    chess_col_alt_8 = chess_col_alt + 2;
+    chess_row_8 = chess_row - 1;
+
+    if (chess_row < 1 || chess_row > 8) || (chess_col ~= 'a' && chess_col ~= 'b' && chess_col ~= 'c' && chess_col ~= 'd' && chess_col ~= 'e' && chess_col ~= 'f' && chess_col ~= 'g' && chess_col ~= 'h')
+        fprintf('toa do khong hop le\n')
+    else
+        if chess_row_8 >= 1 && chess_row_8 <= 8 && chess_col_alt_8 >= 1 && chess_col_alt_8 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_7 >= 1 && chess_row_7 <= 8 && chess_col_alt_7 >= 1 && chess_col_alt_7 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_6 >= 1 && chess_row_6 <= 8 && chess_col_alt_6 >= 1 && chess_col_alt_6 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_5 >= 1 && chess_row_5 <= 8 && chess_col_alt_5 >= 1 && chess_col_alt_5 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_4 >= 1 && chess_row_4 <= 8 && chess_col_alt_4 >= 1 && chess_col_alt_4 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_3 >= 1 && chess_row_3 <= 8 && chess_col_alt_3 >= 1 && chess_col_alt_3 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_2 >= 1 && chess_row_2 <= 8 && chess_col_alt_2 >= 1 && chess_col_alt_2 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        if chess_row_1 >= 1 && chess_row_1 <= 8 && chess_col_alt_1 >= 1 && chess_col_alt_1 <= 8
+            so_nuoc_di = so_nuoc_di + 1;
+        end
+        fprintf('So nuoc di hop le la: ')
+        fprintf('%d\n',so_nuoc_di)
+
+    end
+end
 
  
