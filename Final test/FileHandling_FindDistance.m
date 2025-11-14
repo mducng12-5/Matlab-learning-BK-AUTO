dfbdef6a1856931bbfb6bd55e1bd9b378b2a6dd1
mducng12-5 <minhnguyenduc1205@gmail.com>
2025-11-15 00:17:07 +0700

    xử lý file hỗ trợ tìm distance

---
+++ Final test/FileHandling_FindDistance.m
@@ -0,0 +1,20 @@
+function [Matrix_GPS_Longtitude_1, Matrix_GPS_Lattitude_1, Matrix_GPS_Longtitude_2, Matrix_GPS_Lattitude_2] = FileHandling_FindDistance()
+    file_name_1 = 'C:\My project\Matlab learning bk auto\Final test\Matlab cuối khóa\pickoff_noido_30_03_23.003.tab';
+    opts_1 = detectImportOptions(file_name_1, 'FileType', 'text');
+    opts_1.VariableNamesLine = 10;
+    opts_1.VariableUnitsLine = 11; 
+    opts_1.DataLines = [12, Inf];   
+    data_table_1 = readtable(file_name_1, opts_1);
+    
+    file_name_2 = 'C:\My project\Matlab learning bk auto\Final test\Matlab cuối khóa\pickoff_caotoc_01_04_23.001.tab';
+    opts_2 = detectImportOptions(file_name_2, 'FileType', 'text');
+    opts_2.VariableNamesLine = 10;
+    opts_2.VariableUnitsLine = 11; 
+    opts_2.DataLines = [12, Inf];   
+    data_table_2 = readtable(file_name_2, opts_2);
+    
+    Matrix_GPS_Lattitude_1 =  [data_table_1.CSMOTA_Latitude];
+    Matrix_GPS_Longtitude_1 = [data_table_1.CSMOTA_Longitude];
+    Matrix_GPS_Lattitude_2 =  [data_table_2.CSMOTA_Latitude];
+    Matrix_GPS_Longtitude_2 = [data_table_2.CSMOTA_Longitude];
+end
